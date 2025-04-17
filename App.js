ca141f04d059976c08bc86ff0163dee948a8c05b
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-04-17 08:38:29 +0700

    ฝึก RN: useState/basics

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, Button, Alert } from "react-native";
+import { useState } from "react";
+//import from file
+import { myStyle } from "./styles/myStyle";
+
+const logo = require("./assets/boob.jpg");
 
 export default function App() {
+  const [dev, setDev] = useState({name:"Apo",year:2020})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={myStyle.container}>
+      <Image 
+      source={logo} 
+      style={myStyle.image}></Image>
+      <Image
+        source={{ uri: "https://picsum.photos/id/20/367/267" }}
+        style={myStyle.image}
+      ></Image>
+      <Text style={myStyle.header}>hi girls</Text>
+      <Text style={myStyle.content}>hey me: {dev.name} {dev.year} </Text>
+      <Button title="updated" color="red" onPress={()=>
+        setDev({name:"Nattarisa", year:2025})
+      } 
+      ></Button>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/myStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const myStyle = StyleSheet.create({
+    container: { flex: 1, justifyContent: "center", alignItems: "center" },
+    header: { fontSize: 30, fontWeight: "bold",color:"red" },
+    content: { fontStyle: "italic" },
+    image:{
+      width:300,
+      height:300
+    }
+  });
