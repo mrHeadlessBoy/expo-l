59eca5d1a2f458f3d6aa467d1e4fe39e236cede6
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-04-17 09:13:16 +0700

    ฝึก RN: การออกแบบและการใช้ style

--- components/Kon.js
+++ components/Kon.js
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native"
+import { myStyle } from "../styles/myStyle"
 
 //use props or specific variable
 export default function Kon(props){
     return(
-        <View>
-            <Text>Name {props.name} , Age {props.age}  </Text>
+        <View style={myStyle.content}>
+            <Text style={myStyle.header}>Name {props.name} , Age {props.age}  </Text>
         </View>
     )
 }
--- styles/myStyle.js
+++ styles/myStyle.js
@@ -1,11 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const myStyle = StyleSheet.create({
-    container: { flex: 1, justifyContent: "center", alignItems: "center" },
-    header: { fontSize: 30, fontWeight: "bold",color:"red" },
-    content: { fontStyle: "italic" },
-    image:{
-      width:300,
-      height:300
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "pink",
+    marginVertical: 50,
+    marginHorizontal: 10,
+  },
+  header: { fontSize: 30, fontWeight: "bold", color: "red" },
+  content: {
+    backgroundColor: "orange",
+    padding: 30,
+    marginTop: 15,
+    borderWidth: 2,
+    borderColor: "black",
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
+});
